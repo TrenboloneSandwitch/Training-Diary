8d18709732646d6d92e3fc07bc1aa7bca3051dc8
Bobby Sixkiller <jansoldat68@gmail.com>
2019-01-18 14:37:31 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,124 @@
+var PHASE;
+
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+});
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+});
+
+// Při otevření modalu ulož kde jsi ho otevřel
+$('#addExerciseModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget)
+  var hiddenVal = button.data('phase')
+  PHASE = hiddenVal;
+});
+
+
+
+
+// Class init
+const ui = new UI();
+
+
+// Event listeners
+  // Add exercise listener
+  document.querySelector('#add-series-btn').addEventListener('click', createInputs);
+
+  document.querySelector('#remove-series-btn').addEventListener('click', removeLastRowFromModal);
+  // remove item listener
+  document.querySelector('#container-for-exercises').addEventListener('click', e =>{
+    if(e.target.parentNode.classList.contains('remove')){
+      removeItem(e.target.parentNode.parentNode.parentNode);  
+    };
+  });
+  // Submit form listener
+  document.querySelector('#save-exercise-btn').addEventListener('click', addExercise);
+
+  // Close modal listener
+  document.querySelector('.modal').addEventListener('click', e => {
+    // Close modal if you click 'X' or 'Zavřít'  
+    if (e.target.classList.contains('close-modal')) {
+
+      closeModal();
+    }
+    e.preventDefault();
+  });
+/*************************************************************/
+
+// Add Inputs into modal
+function createInputs(e) {
+  ui.addRowsIntoModal();
+  e.preventDefault();
+}
+
+function removeLastRowFromModal(e){
+  ui.removeLastRowFromModal();
+}
+
+function addExercise(e) {
+  // Rozhodni jestli jde o rozcvičku nebo hlavní část
+  let containerID = '';
+  if (PHASE === 'warmUP') {
+    containerID = 'warmup-exercises-container';
+
+  } else {
+    containerID = 'main-exercises-container';
+  };
+
+  const reps = [];
+  const series = [];
+  const weight = [];
+  const name = document.querySelector('#exercise-name-input').value;
+
+  // Nejdřív zjisti jestli vyplnil jméno
+  if (typeof name !== 'string' || name === '') {
+    ui.showAlert('Název cviku není vyplněn!', 'danger', 'times', '#modal-form', '#exercise-row');
+  } else {
+    // Zjisti počet řádků
+    const numberOfRows = ui.getRowsFromModal().length;
+    let noEmptyFields = true;
+    // Zapiš hodnoty do příslušných řad
+    for (let index = 0; index < numberOfRows; index++) {
+      series[index] = document.querySelector(`#exercise-series-input-${index+1}`).value;
+      reps[index] = document.querySelector(`#exercise-reps-input-${index+1}`).value;
+      weight[index] = document.querySelector(`#exercise-weight-input-${index+1}`).value;
+
+      // Podmínka na vyplnění inputů
+      if (series[index] === '' || reps[index] === '' || weight[index] === '') {
+        ui.showAlert('Vyplňte prosím všechny údaje!', 'danger', 'times', '#modal-form', '#exercise-row');
+        noEmptyFields = false;
+        break;
+      }
+    }
+    // Pokud je vše v pořádku, můžeš pokračovat
+    if (noEmptyFields) {
+      ui.showAlert('Cvik byl úspěšně vložen do seznamu!', 'success', 'check', '#modal-form', '#exercise-row')
+      // Objekt s příslušnýma hodnotama
+      const exercise = {
+        name,
+        series,
+        reps,
+        weight
+      };
+
+
+      // Add into UI
+      ui.addExerciseIntoUI(exercise, containerID);
+      // Add to Storage
+      // Back to default state
+      ui.removeAllAdditionallRowsFromModal();
+    }
+  }
+  e.preventDefault();
+}
+
+function closeModal() {
+  ui.removeAllAdditionallRowsFromModal();
+}
+
+function removeItem(item) {
+  ui.removeExerciseFromUI(item.parentNode);
+  
+ // ui.removeExerciseFromUI(item);  
+}
---
+++ src/js/UIHandler.js
@@ -0,0 +1,197 @@
+class UI {
+    constructor() {
+        this.form = document.querySelector('#modal-form');
+        this.span = document.querySelector('#next-inputs');
+        this.removeRowBtn = document.querySelector('#remove-series-btn');
+    };
+
+    // Add row of inputs into modal
+    addRowsIntoModal() {
+        // Get last index and + one
+        const ID = this.getRowsFromModal().length + 1;
+        // Create div and add classes
+        const div = document.createElement('div');
+        div.classList = 'row mt-1 inputs-row';
+        div.dataset.rowID = this.getRowsFromModal().length + 1;
+        // Ineer HTML
+        let html = `
+        <div class="col-4">
+            <input class="form-control custom-form-input" type="text" placeholder="" id="exercise-series-input-${ID}">
+        </div>
+        <div class="col-4">
+            <input class="form-control" type="text" placeholder="" id="exercise-reps-input-${ID}">
+        </div>
+        <div class="col-4"> 
+            <input class="form-control" type="text" placeholder="" id="exercise-weight-input-${ID}">
+        </div>
+        `;
+        div.innerHTML = html;
+        // Insert created div on certain position
+        this.form.insertBefore(div, this.span);
+
+        this.hideRemoveButton();
+        
+    }
+    // Remove last row from modal form
+    removeLastRowFromModal() {
+        const inputs = this.getRowsFromModal();
+        if (inputs.length > 1) {
+            inputs[inputs.length - 1].parentNode.removeChild(inputs[inputs.length - 1]);
+            this.hideRemoveButton();
+        }
+    }
+    // Remove all rows except default first one
+    removeAllAdditionallRowsFromModal() {
+        let divs = document.querySelectorAll('.inputs-row');
+        // Convert nodelist into array
+        const divsArr = Array.prototype.slice.call(divs);
+        // Loop through the all rows of inputs except the first one
+        for (let index = 1; index < divsArr.length; index++) {
+            // Remove all rows except the first one
+            divsArr[index].remove();
+        }
+        // Clear values from inputs
+        this.clearInputs();
+
+        this.hideRemoveButton();
+    }
+    // Returns node array of all rows
+    getRowsFromModal() {
+        const inputs = document.querySelectorAll('.inputs-row');
+        return inputs;
+    }
+
+    // Clear values from all inputs
+    clearInputs() {
+        // Select every input in the document
+        const nodelist = document.querySelectorAll('input');
+        // Convert nodearray into array
+        const divsArr = Array.prototype.slice.call(nodelist);
+        // Loop through and clear all values
+        for (let index = 0; index < divsArr.length; index++) {
+            divsArr[index].value = '';
+        };
+    }
+    // Create exercise based on user inputs and add it into UI
+    addExerciseIntoUI(exercise, containerID) {
+        const mainDiv = document.querySelector(`#${containerID}`);
+        //mainDiv.classList = 'card-body col-12'
+        const numberOfRows = exercise.reps.length;
+        if (document.querySelector(`#empty-span-${containerID}`)) {
+            mainDiv.innerHTML = '';
+        }
+
+        const subDIV = document.createElement('div');
+
+        // create elements
+        const row = document.createElement('div');
+        const col1 = document.createElement('div');
+        const col2 = document.createElement('div');
+        const hr = document.createElement('hr');
+        // append classes to the elements
+        row.classList = 'row text-dark text-center align-items-center';
+        col1.classList = 'col-5 col-sm-4 col-md-3';
+        col2.classList = 'col-7 col-sm-8 col-md-9 tables';
+
+        col1.innerText = exercise.name;
+        col1.appendChild(document.createElement('br'));
+        const aData = [
+            ['edit', 'warning', 'Upravit záznam', 'pencil'],
+            ['remove', 'danger', 'Smazat záznam', 'trash']
+        ];
+        aData.forEach(item => {
+            // Create linj element
+            const link = document.createElement('a');
+            // Add classes and datasets
+            link.classList = `${item[0]} text-${item[1]} mr-2`;
+            link.dataset.toggle = 'tooltip';
+            link.dataset.placement = 'bottom';
+            link.dataset.trigger = 'hover';
+            link.style.cursor = 'pointer';
+            link.title = `${item[2]}`;
+            // add icon
+            link.innerHTML = `
+                    <i class="fa fa-${item[3]} fa-lg" aria-hidden="true">
+                    </i>`;
+            // Append to collumn
+            col1.appendChild(link);
+        });
+
+        let html = '';
+        for (let index = 0; index < numberOfRows; index++) {
+            html += `
+            <table>
+            <tbody>
+              <tr>
+                <th>${exercise.series[index]}x${exercise.reps[index]}</th>
+              </tr>
+              <tr>
+                <th>${exercise.weight[index]}</th>
+              </tr>
+            </tbody>
+          </table>
+            `;
+        }
+        col2.innerHTML = html;
+
+        // Append cols to rows
+        row.appendChild(col1);
+        row.appendChild(col2);
+        // Append rows to container
+        subDIV.appendChild(row);
+        subDIV.appendChild(hr);
+        mainDiv.appendChild(subDIV);
+    }
+
+    hideRemoveButton(){
+        const inputs = this.getRowsFromModal();
+
+        if (inputs.length === 1) {
+            this.removeRowBtn.style.display = 'none';
+        } else if (inputs.length !== 1 && (this.removeRowBtn.style.display === 'none')){
+            this.removeRowBtn.style.display = 'block';console.log('fire');
+
+        }
+    }
+
+
+    // Remove exercise
+    removeExerciseFromUI(item) {
+        const ID = item.parentNode.id;
+        const div = item.parentNode;
+        item.remove();
+        let nodes = document.querySelector(`#${ID}`).childNodes;
+        if (nodes.length === 0) {
+            let span = document.createElement('span');
+            span.id = 'empty-span-' + ID;
+            span.textContent = 'Zatím nebyl přidán žádný cvik.';
+            div.appendChild(span);
+        }
+    }
+
+
+    // Show alert
+    showAlert(msg, cls, icon, begin, end) {
+        this.clearAlert();
+        const beginNodeUI = document.querySelector(begin);
+        const endNodeUI = document.querySelector(end);
+        const div = document.createElement('div');
+        div.classList = `alert alert-${cls} alert-modal`;
+        div.innerHTML = ` <i class="fa fa-${icon}"></i> ${msg}`;
+        beginNodeUI.insertBefore(div, endNodeUI);
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    // Clear alert
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert-modal');
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+
+}
